22f790532ba82e88be12ee6c0b4c49d81238d878
Marcos Andrade <marcospandrade2@gmail.com>
2025-06-19 21:26:20 -0300

    ci: changes on prisma client at seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,6 @@
-import { PrismaBetterSQLite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "@prisma/client";
 
-const adapter = new PrismaBetterSQLite3({
-  url: process.env.DATABASE_URL || "file:./prisma/dev.db",
-});
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 async function main() {
   const products = [
